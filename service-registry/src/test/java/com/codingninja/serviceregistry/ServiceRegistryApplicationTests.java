8d1cec430925994e5602fafe5466c9b0021aa890
Satyas <surya1999challa@gmail.com>
2024-02-04 14:15:30 +0530

    Initial Commit for Service Registry

---
+++ service-registry/src/test/java/com/codingninja/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingninja.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
